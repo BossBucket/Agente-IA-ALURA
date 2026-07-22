6f4072b15e62b559b8e7b3ac7ea374a97920045a
Ethan <ethanespinoza9@gmail.com>
2026-07-22 16:27:14 -0600

    Añadiendo compatibilidad de la API

--- src/models/my_models.py
+++ src/models/my_models.py
@@ -8,7 +8,7 @@ GOOGLE_API_KEY = st.secrets.get("GOOGLE_API_KEY") or os.getenv("GOOGLE_API_KEY")
 def embeddings():
     """Retorna el modelo de traducción de texto a vectores para la base"""
     if not GOOGLE_API_KEY:
-        raise ValueError("No se encontró la GOOGLE_API_KEY en Secrets ni en las variables de entorno.")
+        raise ValueError("No se encontró la GOOGLE_API_KEY")
         
     return GoogleGenerativeAIEmbeddings(
         model="gemini-3.5-flash",
