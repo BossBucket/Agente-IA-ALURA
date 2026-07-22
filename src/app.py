dde8707beeb25d375a88a0c7316393c0da375ae0
Ethan <ethanespinoza9@gmail.com>
2026-07-22 17:03:36 -0600

    Fix ChromaDB SQLite y regeneracion de vectores

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
-# Parche obligatorio para ChromaDB en Linux / Streamlit Cloud
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 
 
 import sys
