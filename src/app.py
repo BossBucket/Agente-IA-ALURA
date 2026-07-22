e1ec7185002eb168ad6abe087625617f55bf4f6a
Ethan <ethanespinoza9@gmail.com>
2026-07-22 16:39:37 -0600

    compatibilidad de la database

--- src/app.py
+++ src/app.py
@@ -1,3 +1,9 @@
+# Parche obligatorio para ChromaDB en Linux / Streamlit Cloud
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 import sys
 from pathlib import Path
 
